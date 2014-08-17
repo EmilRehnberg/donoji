c0b46bffdd01939ab3bb251a7ad518e7288001ad
Emil Rehnberg <emil.rehnberg@gmail.com>
2014-08-17 08:11:19 +0200

    Added Word#to_s spec

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Donoji::Word do
   it "responds to #hits" do
     expect(word.hits).to eql(3)
   end
+
+  it "formats nicely with #to_s" do
+    expect(word.to_s).to eql("積ん読：3")
+  end
 end
